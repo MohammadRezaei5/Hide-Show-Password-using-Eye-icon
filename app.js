8a51706672b3cc96d86dccc81204b0b30e86b94b
mhmmd <mhmmd.rezaei4@gmail.com>
2023-04-25 11:35:22 +0330

    add project

---
+++ app.js
@@ -0,0 +1,14 @@
+const passwordField = document.getElementById("password");
+const togglePassword = document.querySelector(".password-toggle-icon i");
+
+togglePassword.addEventListener("click", function () {
+  if (passwordField.type === "password") {
+    passwordField.type = "text";
+    togglePassword.classList.remove("fa-eye");
+    togglePassword.classList.add("fa-eye-slash");
+  } else {
+    passwordField.type = "password";
+    togglePassword.classList.remove("fa-eye-slash");
+    togglePassword.classList.add("fa-eye");
+  }
+});
